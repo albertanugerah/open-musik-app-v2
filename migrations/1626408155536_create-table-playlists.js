b3627cd92a932318346ea6d62a9d64ca551932cc
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-07-18 14:42:34 +0700

    update table playlist

--- migrations/1626408155536_create-table-playlists.js
+++ migrations/1626408155536_create-table-playlists.js
@@ -3,39 +3,24 @@
 exports.shorthands = undefined;
 
 exports.up = (pgm) => {
-  pgm.createTable('collaborations', {
+  pgm.createTable('playlists', {
     id: {
       type: 'VARCHAR(50)',
       primaryKey: true,
     },
-    playlist_id: {
-      type: 'VARCHAR(50)',
+    name: {
+      type: 'TEXT',
       notNull: true,
     },
-    user_id: {
+    owner: {
       type: 'VARCHAR(50)',
       notNull: true,
     },
   });
 
-  pgm.addConstraint(
-    'collaborations',
-    'unique_playlist_id_and_user_id',
-    'UNIQUE(playlist_id, user_id)'
-  );
-
-  pgm.addConstraint(
-    'collaborations',
-    'fk_collaborations.playlist_id_playlists.id',
-    'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE'
-  );
-  pgm.addConstraint(
-    'collaborations',
-    'fk_collaborations.user_id_users.id',
-    'FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE'
-  );
+  pgm.addConstraint('playlists', 'fk_playlists.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
-  pgm.dropTable('collaborations');
+  pgm.dropTable('playlists');
 };
